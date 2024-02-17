02841fee1500b12c67318953093f871f2c3b52aa
SAYANTAN GOLDER <144215903+goldersayantan@users.noreply.github.com>
2024-02-17 20:44:12 +0530

    Convert The Temperature from Fahrenheit To Centigrade.py

---
+++ Convert The Temperature from Fahrenheit To Centigrade..py
@@ -0,0 +1,4 @@
+# Convert The Temperature from Fahrenheit To Centigrade...
+f = float(input("Enter the temperature in Fahrenheit: "))
+c = ((f - 32) * 5)/9
+print("The temperature in Centigrade is: ", c)
