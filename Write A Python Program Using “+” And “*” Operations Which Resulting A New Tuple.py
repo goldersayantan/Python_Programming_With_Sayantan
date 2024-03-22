13fde439aec6d23a32251ef5a77fe70fc15b9743
SAYANTAN GOLDER <144215903+goldersayantan@users.noreply.github.com>
2024-03-22 21:25:06 +0530

    Write A Python Program Using “+” And “*” Operations Which Resulting A New Tuple.py

---
+++ Write A Python Program Using “+” And “*” Operations Which Resulting A New Tuple.py
@@ -0,0 +1,6 @@
+# Write A Python Program Using “+” And “*” Operations Which Resulting A New Tuple...
+tup1 = (10, 20, 30, 40, 50, 60, 70, 80, 90)
+tup2 = (100, 200, 300, 400, 500, 600, 700, 800, 900)
+print("The new tuple after using + will be: ", tup1 + tup2)
+print("The new tuple after using * will be: ", tup1 * 2)
+print("The new tuple after using * will be: ", tup2 * 2)
