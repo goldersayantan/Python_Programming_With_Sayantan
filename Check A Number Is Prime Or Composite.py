2c2fb2aeec70c65ae5ae02ef6d489090f819cf54
SAYANTAN GOLDER <144215903+goldersayantan@users.noreply.github.com>
2024-05-26 17:19:10 +0530

    Update Check A Number Is Prime Or Composite.py

--- Check A Number Is Prime Or Composite.py
+++ Check A Number Is Prime Or Composite.py
@@ -1,21 +1,22 @@
-# Check A Number Is Prime Or Composite...
-def prime_composite(x):
-    temp = 0
-    for i in range(x, 1):
-        if x % i == 0:
-            temp = 1
-        else:
-            temp = 2
-
-    if temp == 1:
-        print(x, "is a composite number")
-    elif temp == 0:
-        print(x, "is neither prime nor composite")
-    elif temp == 2:
-        print(x, "is a prime number")
+# Check a number is prime or composite...
+num = int(input("Enter a number to check if it is a prime number: "))
+prime = 0
+for i in range(2, num):
+    if num % i == 0:
+        prime = 0
+        break
     else:
-        print("invalid number")
+        prime = 1
 
-
-num = int(input("Enter a number: "))
-prime_composite(num)
+if num == 0:
+    print("0 is not considered as a prime or composite number")
+elif num == 1:
+    print(f"{num} is neither prime nor a composite number")
+elif num == 2:
+    print(f"{num} is a prime number")
+elif num < 0:
+    print("Negative numbers are not considered as prime or composite numbers")
+elif prime == 0:
+    print(f"{num} is a composite number")
+else:
+    print(f"{num} is a prime number")
