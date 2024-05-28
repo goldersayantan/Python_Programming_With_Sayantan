1c0763235839afaa5c88a851dd83f010b0617809
SAYANTAN GOLDER <144215903+goldersayantan@users.noreply.github.com>
2024-05-28 19:32:51 +0530

    Update Display The Fibonacci Series Using Function.py

--- Display The Fibonacci Series Using Function.py
+++ Display The Fibonacci Series Using Function.py
@@ -12,3 +12,18 @@ def fibonacci(x):
 
 limit = int(input("Enter the number of numbers you want to display in fibonacci series: "))
 fibonacci(limit)
+
+
+
+# Display the fibonacci series using function...
+def fibonacci(x):
+    temp1 = 0
+    temp2 = 1
+    while temp1 < x:
+        print(temp1)
+        temp1, temp2 = temp2, temp1 + temp2
+
+
+limit = int(input("Enter the limit: "))
+print("The fibonacci series will be: ")
+fibonacci(limit)
